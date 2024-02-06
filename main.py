4c387ecf52c8ffdf44f7ad2fd11ce54bcebe827f
Jay <jay.nguyen@students.fhu.edu>
2024-02-05 22:11:22 -0600

    Update main.py
    third test, c and b is defined in function

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def parse_month(month):
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
-def parse_date(a):
+def parse_date(a, b, c):
     a = parse_month(a)
-    date = (a)
+    date = (a, b, c)
     return '/'.join(date)
 
 
@@ -35,10 +35,9 @@ if __name__ == '__main__':
 
     a = a.replace(',','')
     a = a.split()
-
     b = a[1]
     c = a[2]
     a = a[0]
-    results = parse_date(a)
+    results = parse_date(a,b,c)
 
     print(results)
