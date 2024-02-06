2119be02f39141735f97a58d4ef2344990d3870b
Jay <jay.nguyen@students.fhu.edu>
2024-02-05 22:07:16 -0600

    Update main.py
    completed lab , first test

--- main.py
+++ main.py
@@ -3,14 +3,42 @@
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
 def parse_month(month):
-    pass
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    return months[a]
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
-def parse_date(user_string):
-    pass
+def parse_date(a, b, c):
+    a = parse_month(a)
+    date = (a, b, c)
+    return '/'.join(date)
+
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+    a = input()
+
+    a = a.replace(',','')
+    a = a.split()
+    
+    b = a[1]
+    c = a[2]
+    a = a[0]
+    results = parse_date(a,b,c)
+
+    print(results)
